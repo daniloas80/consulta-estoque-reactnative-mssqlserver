26d2f40e1d8a835048c16dca4f4f444b9d042a80
Danilo <daniloalexsilva@gmail.com>
2019-10-26 16:26:11 -0300

    conexão com bd mssql server e exbição da listagem ok

---
+++ server.js
@@ -0,0 +1,58 @@
+const Product = require('./src/app/models/Products');
+
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('db_amoinox', 'sa', 'unidata', {
+  host: '10.211.55.3',
+  dialect: 'mssql',
+});
+
+//sequelize.sync();
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+
+    // sequelize
+    //   .query(
+    //     'SELECT TOP (10) CodProduto, NumProduto, Produto, QuantEstoque, PrecoVenda FROM tab_produto',
+    //     { raw: true }
+    //   )
+    //   .then(projects => {
+    //     console.log(JSON.stringify(projects));
+    //   });
+
+    // Product.findAll({ limit: 10 }).then(project => {
+    //   console.log(JSON.stringify(project));
+    // });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+//   sequelize.close();
+app.get('/produtos', function(req, res) {
+  sequelize
+    .query(
+      'SELECT TOP (10) CodProduto, NumProduto, Produto, QuantEstoque, PrecoVenda FROM tab_produto',
+      { raw: true }
+    )
+    .then(projects => {
+      //console.log(JSON.stringify(projects));
+      res.json(projects);
+    });
+});
+
+const server = app.listen(3333, function() {
+  const host = server.address().address;
+  const { port } = server.address();
+});
---
+++ src/app/models/Products.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Products extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        CodProduto: Sequelize.STRING,
+        NumProduto: Sequelize.STRING,
+        Produto: Sequelize.STRING,
+        QuantEstoque: Sequelize.FLOAT,
+        PrecoVenda: Sequelize.DECIMAL(10, 2),
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+}
+
+export default Products;
